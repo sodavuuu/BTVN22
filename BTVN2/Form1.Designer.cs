92822774e5fc0288cbdec11e6f94fc5c9b895b7b
sondang <sondang@251105@gmail.com>
2025-04-03 17:40:16 +0700

    mới

--- BTVN2/Form1.Designer.cs
+++ BTVN2/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace BTVN2
+﻿namespace ImageViewer
 {
     partial class Form1
     {
@@ -28,13 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.loadFolderButton = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // loadFolderButton
+            // 
+            this.loadFolderButton.Location = new System.Drawing.Point(12, 12);
+            this.loadFolderButton.Name = "loadFolderButton";
+            this.loadFolderButton.Size = new System.Drawing.Size(100, 30);
+            this.loadFolderButton.TabIndex = 0;
+            this.loadFolderButton.Text = "Load Folder";
+            this.loadFolderButton.UseVisualStyleBackColor = true;
+            this.loadFolderButton.Click += new System.EventHandler(this.loadFolderButton_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 48);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(200, 400);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(218, 48);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(570, 370);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(218, 421);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(570, 27);
+            this.label1.TabIndex = 3;
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 460);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.loadFolderButton);
+            this.Name = "Form1";
+            this.Text = "Image Viewer";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button loadFolderButton;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
-
--- BTVN2/Form1.cs
+++ BTVN2/Form1.cs
@@ -1,20 +1,72 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using System.Windows.Forms;
 
-namespace BTVN2
+namespace ImageViewer
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void loadFolderButton_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            var res = dialog.ShowDialog();
+
+            if (res == DialogResult.OK)
+            {
+                var path = dialog.SelectedPath;
+                string[] files = Directory.GetFiles(path);
+
+                // Clear existing controls
+                flowLayoutPanel1.Controls.Clear();
+
+                foreach (var file in files)
+                {
+                    string extension = Path.GetExtension(file).ToLower();
+                    if (extension == ".jpg" || extension == ".png" || extension == ".jpeg")
+                    {
+                        PictureBox pictureBox = new PictureBox();
+                        pictureBox.Cursor = Cursors.Hand;
+                        pictureBox.Width = 100;
+                        pictureBox.Height = 100;
+                        pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                        pictureBox.Tag = file;
+                        pictureBox.Click += PictureBox_Click;
+
+                        try
+                        {
+                            pictureBox.Load(file);
+                            flowLayoutPanel1.Controls.Add(pictureBox);
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show($"Error loading image {file}: {ex.Message}");
+                        }
+                    }
+                }
+            }
+        }
+
+        private void PictureBox_Click(object sender, EventArgs e)
+        {
+            if (sender is PictureBox pictureBox)
+            {
+                string imgPath = pictureBox.Tag.ToString();
+                try
+                {
+                    pictureBox1.Image?.Dispose(); // Clean up previous image
+                    pictureBox1.Load(imgPath);
+                    label1.Text = "File " + imgPath + " is loaded.";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error loading selected image: {ex.Message}");
+                }
+            }
         }
     }
 }
